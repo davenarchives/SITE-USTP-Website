49ce752a1153b83dd5490e88f084b8511f128893
davenarchives <daventheabyss@gmail.com>
2026-02-21 21:32:45 +0800

    chore: update TinaCMS config to use PUBLIC_TINA_CLIENT_ID

--- tina/config.ts
+++ tina/config.ts
@@ -4,7 +4,7 @@ const branch = process.env.GITHUB_BRANCH || process.env.VERCEL_GIT_COMMIT_REF ||
 
 export default defineConfig({
     branch,
-    clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID || null,
+    clientId: process.env.PUBLIC_TINA_CLIENT_ID || null,
     token: process.env.TINA_TOKEN || null,
 
     build: {
